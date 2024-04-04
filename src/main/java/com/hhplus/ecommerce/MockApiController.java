24e30705f6aea75ce1426e259b1db080e0046d45
mihioon <rangee3014@gmail.com>
2024-04-05 00:07:49 +0900

    mock api 추가

--- src/main/java/com/hhplus/ecommerce/MockApiController.java
+++ src/main/java/com/hhplus/ecommerce/MockApiController.java
@@ -1,21 +1,128 @@
 package com.hhplus.ecommerce;
 
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.*;
+
 @RestController
 public class MockApiController {
 
-    @GetMapping("/v1/user/{userId}")
-    public String getUserById(@PathVariable String userId) {
-        // 하드코딩된 사용자 정보 반환
-        return "헬로우 월드";
+    // 잔액충전 / 조회 api
+    @GetMapping("/customer/amount")
+    public ResponseEntity<?> mockApi() {
+        Map<String, Object> response = new HashMap<>();
+
+        ChargeAmountResponse body = new ChargeAmountResponse(1L, "1000");
+
+        response.put("code", "SUCCESS");
+        response.put("message", "충전되었습니다.");
+        response.put("body", body);
+
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/v1/product/{productId}")
-    public String getProductById(@PathVariable String productId) {
-        // 하드코딩된 제품 정보 반환
-        return "Product Name: Example Product, Price: 1000";
+    // 상품 클래스 (내부 클래스로 정의)
+    @Data
+    private static class ChargeAmountResponse {
+        private Long customerId;
+        private String amount;
+
+        public ChargeAmountResponse(Long customerId, String amount) {
+            this.customerId = customerId;
+            this.amount = amount;
+        }
+    }
+
+    // 상품 조회 api
+    @GetMapping("/store/product/{productId}")
+    public ResponseEntity<?> selectProduct() {
+        Map<String, Object> response = new HashMap<>();
+
+        selectProductResponse body = new selectProductResponse(1L, "상품1", "1000", 1L);
+
+        response.put("code", "SUCCESS");
+        response.put("message", "충전되었습니다.");
+        response.put("body", body);
+
+        return ResponseEntity.ok(response);
+    }
+
+    // 상품 클래스 (내부 클래스로 정의)
+    @Data
+    private static class selectProductResponse {
+        private Long productId;
+        private String productNm;
+        private String salePrice;
+        private Long stockQuantity;
+
+        public selectProductResponse(Long productId, String productNm, String salePrice, Long stockQuantity) {
+            this.productId = productId;
+            this.productNm = productNm;
+            this.salePrice = salePrice;
+            this.stockQuantity = stockQuantity;
+        }
+    }
+
+    // 주문 / 결제 api
+    @GetMapping("/payment/checkOut")
+    public ResponseEntity<?> checkOut() {
+        Map<String, Object> response = new HashMap<>();
+
+        CheckOutResponse body = new CheckOutResponse(1000L, "상품1" , 1L);
+
+        response.put("code", "SUCCESS");
+        response.put("message", "충전되었습니다.");
+        response.put("body", body);
+
+        return ResponseEntity.ok(response);
+    }
+
+    // 상품 클래스 (내부 클래스로 정의)
+    @Data
+    private static class CheckOutResponse {
+        private Long productId;
+        private String productNm;
+        private Long quantity;
+
+        public CheckOutResponse(Long productId, String productNm, Long quantity) {
+            this.productId = productId;
+            this.productNm = productNm;
+            this.quantity = quantity;
+        }
+    }
+
+    // 인기 판매 상품 조회 api
+    @GetMapping("/product/popularItem")
+    public ResponseEntity<?> selectPopularItem() {
+        Map<String, Object> response = new HashMap<>();
+
+        List<PopularItemResponse> body = new ArrayList<>();
+        PopularItemResponse item1 = new PopularItemResponse(1000L, "상품1", "1000");
+        PopularItemResponse item2 = new PopularItemResponse(2000L, "상품2", "2000");
+        body.add(item1);
+        body.add(item2);
+
+        response.put("body", body);
+
+        return ResponseEntity.ok(response);
+    }
+
+    // 상품 클래스 (내부 클래스로 정의)
+    @Data
+    private static class PopularItemResponse {
+        private Long productId;
+        private String productNm;
+        private String salePrice;
+
+        public PopularItemResponse(Long productId, String productNm, String salePrice) {
+            this.productId = productId;
+            this.productNm = productNm;
+            this.salePrice = salePrice;
+        }
     }
 }
