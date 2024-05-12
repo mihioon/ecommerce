5fc8ad0cf3a7c54e14008c30f7f250d6bd53bac6
mihioon <rangee3014@gmail.com>
2024-05-12 21:55:24 +0900

    사용하지 않는 패키지 및 파일 삭제

--- src/main/java/com/hhplus/ecommerce/infrastructure/payment/OrderDetailJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hhplus.ecommerce.infrastructure.payment;
-
-import com.hhplus.ecommerce.domain.payment.model.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetailJpaRepository extends JpaRepository<OrderDetail, Long> {
-}
--- src/main/java/com/hhplus/ecommerce/infrastructure/payment/OrderJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hhplus.ecommerce.infrastructure.payment;
-
-import com.hhplus.ecommerce.domain.payment.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderJpaRepository extends JpaRepository<Order, Long> {
-}
