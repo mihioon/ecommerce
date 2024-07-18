6c033c928caa5aab83b5a9ce015d9febc8b69c52
mihioon <rangee3014@gmail.com>
2024-07-18 19:30:15 +0900

    포인트 내역 테이블 유니크 키 오류 수정

--- src/main/java/com/hhplus/ecommerce/infrastructure/customer/entity/PointHistoryEntity.java
+++ src/main/java/com/hhplus/ecommerce/infrastructure/customer/entity/PointHistoryEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "customer_point_history",
-        uniqueConstraints = {@UniqueConstraint(columnNames = {"customerId", "date"})})
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"customerId", "dateTime"})})
 public class PointHistoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
